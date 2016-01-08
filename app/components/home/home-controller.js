23957c9fc2c4a0b504a0de198f3f3788a5d09bd8
Eduard Gómez <edge@gft.com>
2016-01-08 09:23:22 +0100

    custom home

--- app/components/home/home-controller.js
+++ app/components/home/home-controller.js
@@ -1,169 +1,211 @@
 'use strict';
 
 angular.module('App.Controllers')
-  .controller('homeController', function ($scope, RESTFactory, angularGridInstance, $timeout, $sce, $modal) {
+    .service('imageService', function ($q, $http) {
+        this.loadImages = function () {
+            //return $http.jsonp("https://api.flickr.com/services/feeds/photos_public.gne?tags=purse&format=json&jsoncallback=JSON_CALLBACK");
+            return $http.get('http://www.eduardgomez.me/gangashunter_backend/getProducts.php');
+        };
+    })
+    .controller('homeController', function ($scope, RESTFactory, imageService, angularGridInstance, $timeout, $sce, $modal) {
         $scope.searchTxt = '';
-        RESTFactory.readParallelMultipleBatch(['getProducts'])
-            .then(function (responseArray) {
-                // $scope.colors = responseArray[0];
-                // $scope.tallas = responseArray[1];
-                // $scope.marcas = responseArray[2];
-                // $scope.prendas = responseArray[3];
-                $scope.products = responseArray[0];
-                console.log($scope.products);
-                $scope.productsOriginal = responseArray[0];
-                /*imageService.loadImages()
-                    .then(function (data) {
-                        data.data.items.forEach(function (obj) {
-                            var desc = obj.description,
-                                width = desc.match(/width="(.*?)"/)[1],
-                                height = desc.match(/height="(.*?)"/)[1];
+        //  RESTFactory.readParallelMultipleBatch(['getProducts'])
+        //    .then(function (responseArray) {
+        // $scope.colors = responseArray[0];
+        // $scope.tallas = responseArray[1];
+        // $scope.marcas = responseArray[2];
+        // $scope.prendas = responseArray[3];
+        //  $scope.products = responseArray[0];
+        //  console.log($scope.products);
+        //  $scope.productsOriginal = responseArray[0];
+        $scope.searchTxt = '';
 
-                            obj.actualHeight = height;
-                            obj.actualWidth = width;
-                            obj.description = obj.description.split("photo:")[0] + "photo:</p>";
-                            obj.price = Math.floor((Math.random() * 100) + 5);
-                        });
-                        $scope.pics = data.data.items;
-                        $scope.picsOriginal = data.data.items;
+        //apply search and sort method
+        $scope.$watch('searchTxt', function (val, oldValue) {
+            if (val === oldValue) {
+                return;
+            }
+            val = val.toLowerCase();
+            $scope.pics = $scope.picsOriginal.filter(function (obj) {
+                if (obj.description.toLowerCase()
+                    .indexOf(val) !== -1 || obj.name.toLowerCase()
+                    .indexOf(val) !== -1) {
+                    return true;
+                }
+            });
+        });
 
+        $scope.deliberatelyTrustDangerousSnippet = function (text) {
+            return $sce.trustAsHtml(text);
+        };
 
-                    });*/
+        $scope.getImage = function (product) {
+            if (product.imgs.length >= 1) {
+                return 'http://www.eduardgomez.me/gangashunter_backend/uploads/' + product.imgs.split(',')[0];
+            } else {
+                return '../../styles/sass/theme/images/logo.png';
+            }
+        };
+        imageService.loadImages()
+            .then(function (data) {
+                $scope.pics = data.data;
+                $scope.picsOriginal = data.data;
+                console.log(data);
 
                 $scope.myPagingFunction = function () {
-                    $scope.products.push({
-                        price: '135',
-                        tallaName: '240',
+                    $scope.pics.push({
+                        actualHeight: '135',
+                        actualWidth: '240',
                         author: 'nobody@flickr.com (fabulous_redhead)',
                         author_id: '7842562@N07',
                         date_taken: '2015-11-16T21:49:47-08:00',
                         description: 'posteddd a photo',
                         link: 'https://www.flickr.com/photos/fabulous_redhead/22486967574/',
-                        imgs: '11038720_1125224410824632_7664181595818693161_n.jpg',
-                        uploadDate: '2015-11-18T07:24:24Z',
+                        media: {
+                            m: 'http://placehold.it/300x600/E97452/fff'
+                        },
+                        published: '2015-11-18T07:24:24Z',
                         tags: '',
-                        name: 'posteddd'
+                        title: 'posteddd'
                     }, {
-                        price: '135',
-                        tallaName: '240',
+                        actualHeight: '135',
+                        actualWidth: '240',
                         author: 'nobody@flickr.com (fabulous_redhead)',
                         author_id: '7842562@N07',
                         date_taken: '2015-11-16T21:49:47-08:00',
                         description: 'posteddd a photo',
                         link: 'https://www.flickr.com/photos/fabulous_redhead/22486967574/',
-                        imgs: '11038720_1125224410824632_7664181595818693161_n.jpg',
-                        uploadDate: '2015-11-18T07:24:24Z',
+                        media: {
+                            m: 'http://placehold.it/300x600/E97452/fff'
+                        },
+                        published: '2015-11-18T07:24:24Z',
                         tags: '',
-                        name: 'posteddd'
+                        title: 'posteddd'
                     }, {
-                        price: '135',
-                        tallaName: '240',
+                        actualHeight: '135',
+                        actualWidth: '240',
                         author: 'nobody@flickr.com (fabulous_redhead)',
                         author_id: '7842562@N07',
                         date_taken: '2015-11-16T21:49:47-08:00',
                         description: 'posteddd a photo',
                         link: 'https://www.flickr.com/photos/fabulous_redhead/22486967574/',
-                        imgs: '11038720_1125224410824632_7664181595818693161_n.jpg',
-                        uploadDate: '2015-11-18T07:24:24Z',
+                        media: {
+                            m: 'http://placehold.it/300x600/E97452/fff'
+                        },
+                        published: '2015-11-18T07:24:24Z',
                         tags: '',
-                        name: 'posteddd'
+                        title: 'posteddd'
                     }, {
-                        price: '135',
-                        tallaName: '240',
+                        actualHeight: '135',
+                        actualWidth: '240',
                         author: 'nobody@flickr.com (fabulous_redhead)',
                         author_id: '7842562@N07',
                         date_taken: '2015-11-16T21:49:47-08:00',
                         description: 'posteddd a photo',
                         link: 'https://www.flickr.com/photos/fabulous_redhead/22486967574/',
-                        imgs: '11038720_1125224410824632_7664181595818693161_n.jpg',
-                        uploadDate: '2015-11-18T07:24:24Z',
+                        media: {
+                            m: 'http://placehold.it/300x600/E97452/fff'
+                        },
+                        published: '2015-11-18T07:24:24Z',
                         tags: '',
-                        name: 'posteddd'
+                        title: 'posteddd'
                     }, {
-                        price: '135',
-                        tallaName: '240',
+                        actualHeight: '135',
+                        actualWidth: '240',
                         author: 'nobody@flickr.com (fabulous_redhead)',
                         author_id: '7842562@N07',
                         date_taken: '2015-11-16T21:49:47-08:00',
                         description: 'posteddd a photo',
                         link: 'https://www.flickr.com/photos/fabulous_redhead/22486967574/',
-                        imgs: '11038720_1125224410824632_7664181595818693161_n.jpg',
-                        uploadDate: '2015-11-18T07:24:24Z',
+                        media: {
+                            m: 'http://placehold.it/300x600/E97452/fff'
+                        },
+                        published: '2015-11-18T07:24:24Z',
                         tags: '',
-                        name: 'posteddd'
+                        title: 'posteddd'
                     }, {
-                        price: '135',
-                        tallaName: '240',
+                        actualHeight: '135',
+                        actualWidth: '240',
                         author: 'nobody@flickr.com (fabulous_redhead)',
                         author_id: '7842562@N07',
                         date_taken: '2015-11-16T21:49:47-08:00',
                         description: 'posteddd a photo',
                         link: 'https://www.flickr.com/photos/fabulous_redhead/22486967574/',
-                        imgs: '11038720_1125224410824632_7664181595818693161_n.jpg',
-                        uploadDate: '2015-11-18T07:24:24Z',
+                        media: {
+                            m: 'http://placehold.it/300x600/E97452/fff'
+                        },
+                        published: '2015-11-18T07:24:24Z',
                         tags: '',
-                        name: 'posteddd'
+                        title: 'posteddd'
                     }, {
-                        price: '135',
-                        tallaName: '240',
+                        actualHeight: '135',
+                        actualWidth: '240',
                         author: 'nobody@flickr.com (fabulous_redhead)',
                         author_id: '7842562@N07',
                         date_taken: '2015-11-16T21:49:47-08:00',
                         description: 'posteddd a photo',
                         link: 'https://www.flickr.com/photos/fabulous_redhead/22486967574/',
-                        imgs: '11038720_1125224410824632_7664181595818693161_n.jpg',
-                        uploadDate: '2015-11-18T07:24:24Z',
+                        media: {
+                            m: 'http://placehold.it/300x600/E97452/fff'
+                        },
+                        published: '2015-11-18T07:24:24Z',
                         tags: '',
-                        name: 'posteddd'
+                        title: 'posteddd'
                     }, {
-                        price: '135',
-                        tallaName: '240',
+                        actualHeight: '135',
+                        actualWidth: '240',
                         author: 'nobody@flickr.com (fabulous_redhead)',
                         author_id: '7842562@N07',
                         date_taken: '2015-11-16T21:49:47-08:00',
                         description: 'posteddd a photo',
                         link: 'https://www.flickr.com/photos/fabulous_redhead/22486967574/',
-                        imgs: '11038720_1125224410824632_7664181595818693161_n.jpg',
-                        uploadDate: '2015-11-18T07:24:24Z',
+                        media: {
+                            m: 'http://placehold.it/300x600/E97452/fff'
+                        },
+                        published: '2015-11-18T07:24:24Z',
                         tags: '',
-                        name: 'posteddd'
+                        title: 'posteddd'
                     }, {
-                        price: '135',
-                        tallaName: '240',
+                        actualHeight: '135',
+                        actualWidth: '240',
                         author: 'nobody@flickr.com (fabulous_redhead)',
                         author_id: '7842562@N07',
                         date_taken: '2015-11-16T21:49:47-08:00',
                         description: 'posteddd a photo',
                         link: 'https://www.flickr.com/photos/fabulous_redhead/22486967574/',
-                        imgs: '11038720_1125224410824632_7664181595818693161_n.jpg',
-                        uploadDate: '2015-11-18T07:24:24Z',
+                        media: {
+                            m: 'http://placehold.it/300x600/E97452/fff'
+                        },
+                        published: '2015-11-18T07:24:24Z',
                         tags: '',
-                        name: 'posteddd'
+                        title: 'posteddd'
                     }, {
-                        price: '135',
-                        tallaName: '240',
+                        actualHeight: '135',
+                        actualWidth: '240',
                         author: 'nobody@flickr.com (fabulous_redhead)',
                         author_id: '7842562@N07',
                         date_taken: '2015-11-16T21:49:47-08:00',
                         description: 'posteddd a photo',
                         link: 'https://www.flickr.com/photos/fabulous_redhead/22486967574/',
-                        imgs: '11038720_1125224410824632_7664181595818693161_n.jpg',
-                        uploadDate: '2015-11-18T07:24:24Z',
+                        media: {
+                            m: 'http://placehold.it/300x600/E97452/fff'
+                        },
+                        published: '2015-11-18T07:24:24Z',
                         tags: '',
-                        name: 'posteddd'
+                        title: 'posteddd'
                     });
-                    //$scope.productsOriginal = angular.copy($scope.products);
+                    $scope.picsOriginal = angular.copy($scope.pics);
                 };
 
-                $scope.moreInformation = function (product) {
+                $scope.moreInformation = function (pic) {
                     var modalInstance = $modal.open({
                         animation: $scope.animationsEnabled,
-                        templateUrl: 'components/home/detail/productDetail.html',
-                        controller: 'productDetailCtrl',
+                        templateUrl: 'components/busco/detail/buscoProductDetail.html',
+                        controller: 'buscoProductDetailCtrl',
                         size: 'lg',
                         resolve: {
                             item: function () {
-                                return product;
+                                return pic;
                             }
                         }
                     });
@@ -175,28 +217,6 @@ angular.module('App.Controllers')
                     });
                 };
             });
-
-
-        //apply search and sort method
-        $scope.$watch('searchTxt', function (val, oldValue) {
-            if (val === oldValue) {
-                return;
-            }
-            val = val.toLowerCase();
-            $scope.products = $scope.productsOriginal.filter(function (obj) {
-                if (obj.description.toLowerCase()
-                    .indexOf(val) !== -1 || obj.name.toLowerCase()
-                    .indexOf(val) !== -1) {
-                    return true;
-                }
-            });
-            console.log($scope.products.length+"__"+$scope.productsOriginal.length);
-        });
-
-        $scope.deliberatelyTrustDangerousSnippet = function (text) {
-            return $sce.trustAsHtml(text);
-        };
-
     })
     .controller('productDetailCtrl', function ($scope, $modalInstance, item) {
 
