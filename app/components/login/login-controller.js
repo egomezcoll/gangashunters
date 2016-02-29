452127c790d6084530960e9bb2f5e4e03b85cc88
Eduard Gómez <edge@gft.com>
2016-02-29 13:28:51 +0100

    login awesome

--- app/components/login/login-controller.js
+++ app/components/login/login-controller.js
@@ -1,6 +1,19 @@
 'use strict';
 
 angular.module('App.Controllers')
-    .controller('loginController', function ($scope) {
-
+    .controller('loginController', function ($scope, $timeout) {
+        $timeout(function () {
+            $('.login')
+                .slick({
+                    dots: false,
+                    infinite: true,
+                    speed: 750,
+                    fade: true,
+                    arrows: false,
+                    autoplay: true,
+                    autoplaySpeed: 5000,
+                    cssEase: 'linear',
+                    initialSlide: Math.floor(Math.random() * 10) + 1
+                });
+        }, 500);
     });
