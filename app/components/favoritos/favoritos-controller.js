823c1d7af590d9784e1a6019f3ec83288f107bc5
Eduard Gómez <edge@gft.com>
2016-03-15 10:20:39 +0100

    favoritos done

--- app/components/favoritos/favoritos-controller.js
+++ app/components/favoritos/favoritos-controller.js
@@ -2,12 +2,27 @@
 
 angular.module('App.Controllers')
     .service('favoritosService', function ($http, RESTFactory, geolocation) {
-        this.loadFavorites = function (pag, filters) {
+        this.loadFavorites = function () {
             return geolocation.getLocation()
                 .then(function (data) {
                     return $http.get('http://www.eduardgomez.me/gangashunter_backend/getFavoritos.php?id=1');
                 });
+        };
 
+        this.removeFavorite = function (id) {
+            return $http({
+                method: 'POST',
+                url: 'http://www.eduardgomez.me/gangashunter_backend/deleteFavorito.php',
+                data: {
+                    'id': id,
+                    'idUser': '1'
+                },
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+                    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Allow-Methods'
+                }
+            });
         };
     })
     .controller('favoritosController', function ($scope, RESTFactory, favoritosService, angularGridInstance, $timeout, $filter, $sce, $modal, ngGeodist, cfpLoadingBar, geolocation) {
@@ -48,9 +63,24 @@ angular.module('App.Controllers')
                 return distance + ' km';
             }
         };
+
+        $scope.removeFromFavorites = function (id, index) {
+            favoritosService.removeFavorite(id)
+                .then(function (response) {
+                    if (response.status === 200) {
+                        $scope.pics.splice(index, 1);
+                        sweetAlert({
+                            title: '¡Hecho!',
+                            text: 'Hemos eliminado este producto de tus favoritos',
+                            type: 'success',
+                            showCancelButton: false,
+                        });
+                    }
+                });
+        };
         cfpLoadingBar.start();
 
-        favoritosService.loadFavorites(0)
+        favoritosService.loadFavorites()
             .then(function (data) {
                 $scope.pics = data.data;
                 cfpLoadingBar.complete();
--- app/components/home/detail/productDetail-controller.js
+++ app/components/home/detail/productDetail-controller.js
@@ -6,27 +6,46 @@ angular.module('App.Controllers')
         $scope.item = item;
         $scope.zoomWidth = 300;
         $scope.zoomHeight = 400;
+        $scope.isFavorite = false;
         console.log(item);
-        setTimeout(function(){
-          $('.cloud-zoom, .cloud-zoom-gallery').CloudZoom();
+        setTimeout(function () {
+            $('.cloud-zoom, .cloud-zoom-gallery')
+                .CloudZoom();
+        }, 3000);
+        $http({
+                method: 'POST',
+                url: 'http://www.eduardgomez.me/gangashunter_backend/checkIsFavorito.php',
+                data: {
+                    'idProduct': item.id,
+                    'idUser': '1'
+                },
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+                    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Allow-Methods'
+                }
+            })
+            .then(function (response) {
+                if (response.status === 200) {
+                    if (response.data.length > 0) {
+                        $scope.isFavorite = true;
+                    }
+                }
+            });
 
-        },3000);
         NgMap.getMap()
             .then(function (map) {
                 map.setCenter(new google.maps.LatLng(item.latitude, item.longitude));
                 google.maps.event.trigger(map, 'resize');
                 var img = document.getElementById('img');
 
-                if(img.naturalWidth < $scope.zoomHeight){
-                  $scope.zoomHeight = img.naturalWidth;
+                if (img.naturalWidth < $scope.zoomHeight) {
+                    $scope.zoomHeight = img.naturalWidth;
                 }
 
-                if(img.naturalWidth < $scope.zoomWidth){
-                  $scope.zoomWidth = img.naturalWidth;
+                if (img.naturalWidth < $scope.zoomWidth) {
+                    $scope.zoomWidth = img.naturalWidth;
                 }
-                console.log(img.clientWidth);
-                console.log(img.clientHeight);
-
             });
 
         $scope.getImage = function (product) {
@@ -37,15 +56,42 @@ angular.module('App.Controllers')
                 return '../../styles/sass/theme/images/default.jpg';
             }
         };
+        $scope.addToFavorites = function (id) {
+            $http({
+                    method: 'POST',
+                    url: 'http://www.eduardgomez.me/gangashunter_backend/insertNewFavorito.php',
+                    data: {
+                        'idProduct': id,
+                        'idUser': '1'
+                    },
+                    headers: {
+                        'Access-Control-Allow-Origin': '*',
+                        'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+                        'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Allow-Methods'
+                    }
+                })
+                .then(function (response) {
+                    if (response.status === 200) {
+                        sweetAlert({
+                            title: '¡Hecho!',
+                            text: 'Hemos añadido este producto a tus favoritos',
+                            type: 'success',
+                            showCancelButton: false,
+                        });
+                        $scope.isFavorite = true;
+                    }
+                });
+        };
+
         $scope.ok = function () {
             var product = {
                 "idProduct": item.id,
                 "productName": item.name,
                 "price": item.price,
-                "userId":1,
-                "userName":'Eduard',
-                "idUserOwner":item.idUser,
-                "nameOwner":item.userName
+                "userId": 1,
+                "userName": 'Eduard',
+                "idUserOwner": item.idUser,
+                "nameOwner": item.userName
             };
             $http({
                     method: 'POST',
@@ -58,10 +104,12 @@ angular.module('App.Controllers')
                     }
                 })
                 .then(function (response) {
-                  console.log(response);
+                    console.log(response);
                     if (response.status === 200) {
                         $modalInstance.close();
-                        $state.go('inbox',{'id':response.data[0].id});
+                        $state.go('inbox', {
+                            'id': response.data[0].id
+                        });
                     }
                 });
         };
